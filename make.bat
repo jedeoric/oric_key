bf38a7c992c96f7ffe7f186a2d7f604c4c4d4dd9
jede <jede@oric.org>
2016-10-16 17:46:28 +0200

    managing Orix

--- make.bat
+++ make.bat
@@ -28,11 +28,20 @@ set  execute[1]=make.bat
 set  binary[1]=release\*.com
 set  readme[1]=readme.md
 
+set  repo[2]=https://github.com/jedeoric/stormlord.git
+set  directory[2]=stormlord
+set  targetdirectory[2]=stormld
+set  execute[2]=make.bat
+set  binary[2]=release\*.com
+set  readme[2]=readme.md
+
+
+
 cd ..
 cd build
 
 ::read it using a FOR /L statement
-for /l %%n in (0,1,1) do (
+for /l %%n in (0,1,2) do (
 rem rmdir 4kkong
 echo Generating !binary[%%n]!
 mkdir ..\release\apps\!targetdirectory[%%n]!
@@ -47,6 +56,17 @@ copy !binary[%%n]! ..\..\release\apps\!targetdirectory[%%n]!\
 copy !readme[%%n]! ..\..\release\apps\!targetdirectory[%%n]!\
 cd ..
 )
+
+echo Generating files for bin
+git clone https://github.com/jedeoric/ORIX.git
+cd ORIX
+call build.bat NORUN
+copy release\bin\* ..\release\bin\
+mkdir ..\release\usr\
+mkdir ..\release\usr\share\
+mkdir ..\release\usr\share\man\
+
+
 
 
 
